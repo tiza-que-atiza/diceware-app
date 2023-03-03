907447eac8821d2b8b11fe5dab9f676998dd842a
tiza-que-atiza <126802627+tiza-que-atiza@users.noreply.github.com>
2023-03-03 10:29:56 -0600

    Update diceware.py
    Now the app can handle non integer input and can restart many times.

--- diceware.py
+++ diceware.py
@@ -1,28 +1,43 @@
 import random  # importa random
 import csv
 
-palabras = int(input("¿De cuántas palabras desea que sea su contraseña? $ "))
+def init():
+    try:
+        palabras = int(input("¿De cuántas palabras desea que sea su contraseña? $ "))
+        lista = []
 
-lista = []
 
-for i in range (0, palabras):
+        for i in range (0, palabras):
+            
+            dados = random.sample(range(1,7),5)  # genera 5 números entre 1 y 6
+
+            numero = "".join(map(str,dados))  # convierte los 5 números en un string
+
+            diccionario = {}  # genera un diccionario vacío
+
+            with open('palabras1.csv', mode='r') as inp:  # abre el archivo csv
+                reader = csv.reader(inp)  # toma inputs del csv
+                diccionario = {rows[0]:rows[1] for rows in reader}  # toma inputs de cada columna
+                
+         
+            
+            lista.append(diccionario[numero])
+            
+
+        resultado = ' '.join([str(elem) for elem in lista])
+
+        print(resultado)
+
+
+    except ValueError:
+        print("sexo indeed")
+
+init()
+
+restart = input("¿Quiere generar otra contraseña? (S/n) $ ")
+
+if str(restart) == str("S"):
+    init()
+elif str(restart) == str("n"):
+    print("Gracias por utilizar mi programa.")
     
-    dados = random.sample(range(1,7),5)  # genera 5 números entre 1 y 6
-
-    numero = "".join(map(str,dados))  # convierte los 5 números en un string
-
-    diccionario = {}  # genera un diccionario vacío
-
-    with open('palabras1.csv', mode='r') as inp:  # abre el archivo csv
-        reader = csv.reader(inp)  # toma inputs del csv
-        diccionario = {rows[0]:rows[1] for rows in reader}  # toma inputs de cada columna
-        
- 
-    
-    lista.append(diccionario[numero])
-    
-
-resultado = ' '.join([str(elem) for elem in lista])
-
-print(resultado)
-
