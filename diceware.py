0f5da63102eb52cf85da85824d5ec2354fb5b1b4
tiza-que-atiza <126802627+tiza-que-atiza@users.noreply.github.com>
2023-03-02 21:03:39 -0600

    Update diceware.py
    Now it exports the entire password into a single string.

--- diceware.py
+++ diceware.py
@@ -3,18 +3,26 @@ import csv
 
 palabras = int(input("¿De cuántas palabras desea que sea su contraseña? $ "))
 
+lista = []
+
 for i in range (0, palabras):
     
     dados = random.sample(range(1,7),5)  # genera 5 números entre 1 y 6
 
     numero = "".join(map(str,dados))  # convierte los 5 números en un string
 
+    diccionario = {}  # genera un diccionario vacío
 
-    diccionario = {}
+    with open('palabras1.csv', mode='r') as inp:  # abre el archivo csv
+        reader = csv.reader(inp)  # toma inputs del csv
+        diccionario = {rows[0]:rows[1] for rows in reader}  # toma inputs de cada columna
+        
+ 
+    
+    lista.append(diccionario[numero])
+    
 
-    with open('palabras1.csv', mode='r') as inp:
-        reader = csv.reader(inp)
-        diccionario = {rows[0]:rows[1] for rows in reader}
+resultado = ' '.join([str(elem) for elem in lista])
 
+print(resultado)
 
-    print(diccionario[numero])
